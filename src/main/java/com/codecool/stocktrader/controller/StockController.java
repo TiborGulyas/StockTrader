7fca756517ba1b1138680bc0d336a92ce035d287
TiborGulyas <gulyastibor@gmail.com>
2021-01-28 10:44:58 +0100

    getMapping: get candle data per resolution implemented

--- src/main/java/com/codecool/stocktrader/controller/StockController.java
+++ src/main/java/com/codecool/stocktrader/controller/StockController.java
@@ -32,7 +32,31 @@ public class StockController {
     private ReactCandleDataConverter reactCandleDataConverter;
 
     @GetMapping("/getcandle/{symbol}/{resolution}")
-    public ReactCandleContainer returnIntraday(@PathVariable("symbol") String symbol, @PathVariable("resolution") String res) {
+    public ReactCandleContainer returnIntradayPerRes(@PathVariable("symbol") String symbol, @PathVariable("resolution") String res) {
+        Stock stock = stockRepository.findBySymbol(symbol);
+
+        Resolution resolution = resolutionProvider.createResolution(res);
+        //Resolution resolution5 = resolutionProvider.createResolution("5");
+        //Resolution resolutionD = resolutionProvider.createResolution("D");
+
+        CandleContainer candleReturn = candleRepository.findAllByStockAndResolution(stock, resolution);
+        //CandleContainer candleReturn5 = candleRepository.findAllByStockAndResolution(stock, resolution5);
+        //CandleContainer candleReturnD = candleRepository.findAllByStockAndResolution(stock, resolutionD);
+
+        ReactCandleContainer reactCandleContainer = new ReactCandleContainer();
+
+        reactCandleDataConverter.convertCandleData(candleReturn, resolution, reactCandleContainer);
+        //reactCandleDataConverter.convertCandleData(candleReturn5, resolution5, reactCandleContainer);
+        //reactCandleDataConverter.convertCandleData(candleReturnD, resolutionD, reactCandleContainer);
+
+        System.out.println("react candle sent out: -----------------------");
+        System.out.println(reactCandleContainer);
+        return reactCandleContainer;
+
+    }
+
+    @GetMapping("/getcandle/{symbol}")
+    public ReactCandleContainer returnIntraday(@PathVariable("symbol") String symbol) {
         Stock stock = stockRepository.findBySymbol(symbol);
 
         Resolution resolution1 = resolutionProvider.createResolution("1");
@@ -54,6 +78,8 @@ public class StockController {
         return reactCandleContainer;
 
     }
+
+
 
     @GetMapping("/getquote/{symbol}")
     public LastPrice returnCurrentPrice(@PathVariable("symbol") String symbol) {
